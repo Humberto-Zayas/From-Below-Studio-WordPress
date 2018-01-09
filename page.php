dc0eaad255aba996c9189bd37763d09c374a74e3
Humberto Zayas <soundsinhd@gmail.com>
2018-01-09 18:00:06 -0500

    Begin Separating Templates and Parts
    Split up templates so page.php can handle all the necessary templates

--- page.php
+++ page.php
@@ -23,8 +23,14 @@ get_header(); ?>
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 
+			if (is_front_page()) {
+				get_template_part( 'template-parts/content', 'home' ); //new
+			} else {
+				get_template_part( 'template-parts/content', 'page' );
+			}
+			
 			// Include the page content template.
-			get_template_part( 'template-parts/content', 'page' );
+			//get_template_part( 'template-parts/content', 'page' ); //old
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
