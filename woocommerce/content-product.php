7ca02c83d0d9301d269fe4c7cc4fe317b7ceee9e
Humberto Zayas <humberto@t2marcom.com>
2019-11-03 20:01:43 -0500

    Update content-product.php
    Split products into two column layout

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -27,7 +27,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php post_class(); ?>>
+<li class="w-col w-col-6" <?php post_class(); ?>>
+
 	<?php
 	/**
 	 * woocommerce_before_shop_loop_item hook.
