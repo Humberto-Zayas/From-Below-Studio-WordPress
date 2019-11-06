eba676b8be5b4d9768d807a67ab44a06035b16e5
Humberto Zayas <humberto@t2marcom.com>
2019-11-05 22:47:08 -0500

    Spacing for Product Page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -33,14 +33,14 @@ get_header( 'shop' ); ?>
 		do_action( 'woocommerce_before_main_content' );
 	?>
 
-    <header class="woocommerce-products-header">
+    <header style="padding-top: 70px" class="woocommerce-products-header">
 
 		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
 			<!-- <h1 class="woocommerce-products-header__title page-title"> -->
                 <?php //woocommerce_page_title(); ?>
             <!-- </h1> -->
-            <h1 class="woocommerce-products-header__title page-title">
+            <h1 style="padding-bottom: 40px" class="heading-13 woocommerce-products-header__title page-title">
                 Book Your Recording Session
             </h1>
 
